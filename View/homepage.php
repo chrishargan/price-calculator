21735c073748eeb2ea53ba49ed85995d773bc98f
becode <yf_be@yahoo.co.uk>
2020-08-04 10:31:20 +0200

    added database yuri

--- View/homepage.php
+++ View/homepage.php
@@ -4,6 +4,7 @@ Anything complex should be calculated in the model -->
 <section>
     <h4>Hello <?php echo $user->getName()?>,</h4>
 
+
     <p><a href="index.php?page=info">To info page</a></p>
 
     <p>Put your content here.</p>
